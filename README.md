0413c6e0c1b0ee07d915f2ccefc1888f76dcbd2a
zhaoyk10 <zhaoyk10@gmail.com>
2015-03-02 09:24:56 +0800

    build openssl first

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # sqlcipher_android
 
 build sqlcipher for android 
+
+build openssl http://wiki.openssl.org/index.php/Android#OpenSSL_Library
--- external/Android.mk
+++ external/Android.mk
@@ -11,6 +11,13 @@ android_sqlite_cflags :=  -DHAVE_USLEEP=1 \
 
 sqlcipher_cflags := -DSQLITE_HAS_CODEC -DHAVE_FDATASYNC=0 -Dfdatasync=fsync 
 
+sqlcipher/sqlite3.c:
+	cd $(LOCAL_PATH)/sqlcipher && ./configure
+	make -C sqlcipher sqlite3.c
+
+sqlcipher_files := \
+	sqlcipher/sqlite3.c
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := static-libcrypto
 LOCAL_EXPORT_C_INCLUDES := openssl/include
@@ -20,8 +27,8 @@ include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := sqlite3
-LOCAL_SRC_FILES         := ../sqlite3.c
-LOCAL_C_INCLUDES        := ../
+LOCAL_SRC_FILES         := $(sqlcipher_files)
+LOCAL_C_INCLUDES        := sqlcipher
 LOCAL_CFLAGS            := $(android_sqlite_cflags) $(sqlcipher_cflags)
 LOCAL_STATIC_LIBRARIES 	:= static-libcrypto
 include $(BUILD_SHARED_LIBRARY)
